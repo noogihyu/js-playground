f839df51e92d6c05c205c476eb2ab86a988bf598
noogihyu <noogihyu@hotmail.com>
2018-08-24 19:31:29 +0900

    HTML, JAVASCRIPT 분리, 버튼 예제 등

---
+++ add_button.js
@@ -0,0 +1,22 @@
+//console.log("button js");
+var readInput = function() {
+    var input = document.getElementById('input1');
+    console.log(input.value);
+};
+
+var el = document.getElementById('test2');
+var menu = document.createElement('ul');
+menu.id='menupan';
+
+var item1 = document.createElement('li');
+item1.id = 'menu1';
+item1.innerHTML = "설렁탕";
+
+var item2 = document.createElement('li');
+item2.id = 'menu2';
+item2.innerHTML = "추어탕";
+
+menu.appendChild(item1);
+menu.appendChild(item2);
+
+el.appendChild(menu);
